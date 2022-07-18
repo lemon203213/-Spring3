0a0271189bc0ad1817dd440bb83ca1bb840b96e6
lemon203213 <lemon203213@gmail.com>
2022-07-18 21:14:54 +0900

    생성

---
+++ src/main/java/com/example/week5springhwk/Week5springhwkApplication.java
@@ -0,0 +1,13 @@
+package com.example.week5springhwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5springhwkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week5springhwkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/week5springhwk/Week5springhwkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week5springhwk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5springhwkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
